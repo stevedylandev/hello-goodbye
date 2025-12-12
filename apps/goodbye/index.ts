dab61e5c7a9168f1133b14b2c73536132117bb73
Steve <contact@stevedylan.dev>
2025-12-12 14:29:23 -0500

    chore: updated goodbye

--- apps/goodbye/index.ts
+++ apps/goodbye/index.ts
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const name = args.join(" ");
 
 if (name) {
-	console.log(`Goodbye ${name}!`);
+	console.log(`Goodbye ${name}! We'll miss you!`);
 } else {
 	console.log("Goodbye my buddy!");
 }
